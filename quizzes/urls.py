0ff266a9dba3db965849e0f063c6e1c025525370
Andrew Bilello <andrewbilello@gmail.com>
2021-09-22 10:40:06 -0400

    added edit quiz/question/answer pages with urls and functions

--- quizzes/urls.py
+++ quizzes/urls.py
@@ -18,4 +18,11 @@ urlpatterns = [
     path('<int:quiz_id>/create_question', views.create_question),
     path('<int:quiz_id>/<int:question_id>/new_answer', views.new_answer),
     path('<int:quiz_id>/<int:question_id>/create_answer', views.create_answer),
+    path('<int:quiz_id>/edit_quiz', views.edit_quiz),
+    path('<int:quiz_id>/delete_quiz', views.delete_quiz),
+    path('<int:quiz_id>/update_quiz', views.update_quiz),
+    path('<int:quiz_id>/delete_question/<int:question_id>', views.delete_question),
+    path('<int:quiz_id>/edit_question/<int:question_id>', views.edit_question),
+    path('<int:quiz_id>/<int:question_id>/delete_answer/<int:answer_id>', views.delete_answer),
+    path('<int:quiz_id>/edit_question/create_answer/<int:question_id>', views.create_answer),
 ]
